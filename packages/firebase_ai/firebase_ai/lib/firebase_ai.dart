e93435fa536063d5e60ef40b755394e1ca60248f
Jaime Blasco <jaime@sparkli.ai>
2025-07-04 14:10:28 +0000

    feat: allow transcriptions

--- packages/firebase_ai/firebase_ai/lib/firebase_ai.dart
+++ packages/firebase_ai/firebase_ai/lib/firebase_ai.dart
@@ -105,6 +105,7 @@ export 'src/live_api.dart'
         LiveServerMessage,
         LiveServerContent,
         LiveServerToolCall,
+        Transcription,
         LiveServerToolCallCancellation,
         LiveServerResponse,
         Transcription;
